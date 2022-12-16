ec345f3f759ba1819f45a6e6ff83a244c54ef0c9
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2022-12-16 16:26:36 +0700

    none

--- exp0/Datapipeline.py
+++ exp0/Datapipeline.py
@@ -1,21 +1,24 @@
 def Datapipeline():
     from sklearn.compose import ColumnTransformer
-    from sklearn.tree import DecisionTreeClassifier
     from sklearn.impute import SimpleImputer
     from sklearn.pipeline import Pipeline
     from sklearn.preprocessing import OneHotEncoder
+    from sklearn.preprocessing import StandardScaler
 
     cat_cols = ['Payment', 'City_Tier', 'Gender', 'account_segment', 'Marital_Status', 'Login_device']
     cat_transformer = Pipeline(
         steps=[
             ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("onehot", OneHotEncoder()),
+            ("onehot", OneHotEncoder())
         ]
     )
 
     num_cols = ['Tenure', 'CC_Contacted_LY', 'Service_Score', 'Account_user_count','CC_Agent_Score', 'rev_per_month', 'Complain_ly','rev_growth_yoy', 'coupon_used_for_payment', 'Day_Since_CC_connect','cashback']
     num_transformer = Pipeline(
-        steps=[("imputer", SimpleImputer(strategy="most_frequent"))]
+        steps=[
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("scale", StandardScaler())
+        ]
     )
 
     preprocessor = ColumnTransformer(
